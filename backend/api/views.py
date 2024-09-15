b1b829ab1ce8bf842ecacdf4aa0f9316bbddec57
sruthijaganmohan <sruthi12jagan@gmail.com>
2024-09-15 12:05:39 +0530

    get method on model instance with drf

---
+++ backend/api/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import json
+from products.models import Product
+from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from products.serializers import ProductSerializer
+
+# Create your views here.
+@api_view(["GET"])
+def api_home(request):
+    instance = Product.objects.all().order_by("?").first()
+    data = {}
+    if instance:
+        # data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+        data = ProductSerializer(instance).data
+    return Response(data)
